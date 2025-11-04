24ca1ded7e8a3c48c815419179fea508e2e9824c
Luca Murgia <murgialuca50@gmail.com>
2025-11-04 19:46:16 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4166392623-8d79f",
+  "appId": "1:189875194603:web:ecc822df7212a673b04cae",
+  "apiKey": "AIzaSyDLc9C9KGh_2aF7fzELcgfmH420aBNwchQ",
+  "authDomain": "studio-4166392623-8d79f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "189875194603"
+};
